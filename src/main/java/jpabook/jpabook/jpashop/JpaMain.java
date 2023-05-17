4846ebe0c6f978fccd25b9b1b382e54d8f1e14c8
jnjongjeon <jeonij23@gmail.com>
2023-05-17 14:27:11 +0900

    5/17

---
+++ src/main/java/jpabook/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,24 @@
+package jpabook.jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        tx.commit();
+
+        em.close();
+        emf.close();
+
+    }
+}
---
+++ src/main/java/jpabook/jpabook/jpashop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package jpabook.jpabook.jpashop.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
